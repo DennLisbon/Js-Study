cec99348714828251196d6106482bab2fb362c00
DennLisbon <dennislisboa@protonmail.com>
2026-07-04 02:16:39 -0300

    -m booleans and numbers

---
+++ variables-and-strings/trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Byte";
+const botLocation = "the Cloud";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was originally created in just 10 days by Brendan Eich!";
+console.log(codingFact);
+
+codingFact = "Even though it has 'Java' in its name, " + favoriteLanguage + " is completely unrelated to the Java programming language.";
+console.log(codingFact);
+
+codingFact = "Almost all modern web browsers rely heavily on " + favoriteLanguage + " to make web pages interactive.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
